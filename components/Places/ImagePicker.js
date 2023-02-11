d316a70c5fc5cb20928595e5d465b5d66b26b6ed
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-02-11 05:15:15 +0100

    Use device's camera

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,3 +1,53 @@
-function ImagePicker() {}
+import { Alert, Button, Text, View } from 'react-native'
+import {
+	launchCameraAsync,
+	useCameraPermissions,
+	PermissionStatus
+} from 'expo-image-picker'
+
+function ImagePicker() {
+	const [cameraPesiossionInformation, requestPermission] =
+		useCameraPermissions()
+
+	async function verifyPermissions() {
+		if (cameraPesiossionInformation.status === PermissionStatus.UNDETERMINED) {
+			const permissionResponse = await requestPermission()
+
+			return permissionResponse.granted
+		}
+
+		if (cameraPesiossionInformation.status === PermissionStatus.DENIED) {
+			Alert.alert(
+				'Insuficient Permissions!',
+				'You need to grant camera permissions to use this app.'
+			)
+			return false
+		}
+
+		return true
+	}
+
+	async function takeImageHandler() {
+		const hasPermission = await verifyPermissions()
+
+		if (!hasPermission) {
+			return
+		}
+
+		const image = await launchCameraAsync({
+			allowsEditing: true,
+			aspect: [16, 9],
+			quality: 0.5
+		})
+		console.log(image)
+	}
+
+	return (
+		<View>
+			<View></View>
+			<Button title="Take Image" onPress={takeImageHandler} />
+		</View>
+	)
+}
 
 export default ImagePicker
